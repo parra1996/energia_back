58c476413b437afb5eb7ed987dcc35b99f07d063
“juanP1996” <parrajp1996@gmail.com>
2022-09-14 14:31:25 +0200

    feat: api levantada en local

---
+++ router.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const UsersRouter = require('./views/UsersRouter');
+
+router.use('/users', UsersRouter);
+
+module.exports = router;
---
+++ views/UsersRouter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const UsersController = require("../controllers/UsersController");
+
+router.get("/que", UsersController.quefue) ; 
+
+// router.post("/", UsersController.userRegister);
+
+// router.get("/", UsersController.allUser);
+
+// router.delete("/", UsersController.userDelete);
+
+// router.put("/", UsersController.userUpdate);
+
+// router.get("/profile", UsersController.userProfile);
+
+// router.post("/followed", UsersController.userfollowed);
+
+// router.delete("/followed", UsersController.userUnfollow);
+
+// router.post("/login", UsersController.userLogin);
+
+// router.post("/results/:userName", UsersController.userSearchByUserName);
+
+module.exports = router;
