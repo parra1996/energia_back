60cbc22a905cbc84fa56c14b9ad986d7420de07a
“juanP1996” <parrajp1996@gmail.com>
2022-09-17 17:44:05 +0200

    feat: intento de deploy

--- db.js
+++ db.js
@@ -1,11 +1,6 @@
 const db = () => {
     const mongoose = require('mongoose');
-    // const clave = process.env.DB_CLAVE;
-    // const database = process.env.DB_DATABASE;
-    // const usuario = process.env.DB_USUARIO;
-    // const cluster = process.env.DB_CLUSTER;
-
-    const url = `mongodb+srv://juan:1234@cluster0.oyrs41k.mongodb.net/?retryWrites=true&w=majority`;
+    const url = `mongodb+srv://juan:1234@cluster0.oyrs41k.mongodb.net/test`;
 
     mongoose.connect(url, {
         useNewUrlPArser: true,
