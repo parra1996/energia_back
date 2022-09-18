eef19ef262c41266a95cb540998facb580b09fc5
“juanP1996” <parrajp1996@gmail.com>
2022-09-18 13:32:47 +0200

    feat: agregando uri

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const db = () => {
     const mongoose = require('mongoose');
-    const url = `mongodb+srv://juan:1234@cluster0.oyrs41k.mongodb.net/test?retryWrites=true&w=majority`;
+    const URI = `mongodb+srv://juan:1234@cluster0.oyrs41k.mongodb.net/test?retryWrites=true&w=majority`;
 
-    mongoose.connect(url, {
+    mongoose.connect(URI, {
         useNewUrlPArser: true,
         useUnifiedTopology: true,
     }).then(() => console.log('Database creada')).catch(error => console.log('Algun error ha ocurido', error))
